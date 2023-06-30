a2d4653b0bad08b0ae6112531b512f7920a82800
Ella4040 <usouella@gmail.com>
2023-06-30 16:13:23 -0700

    prints

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,29 +1,19 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
-* Return: the address of the new element, or NULL if it fails
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
-list_t *add_node(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *temp;
 
-	while (str[len])
-		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
-
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
-	return (*head);
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
